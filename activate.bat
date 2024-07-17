c23b5c40454a8f45ce30eb916ccc332cc3e19a4b
Aric-prog <hernandoaric@gmail.com>
2024-07-17 16:49:19 +1000

    yea

---
+++ activate.bat
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./css/index.css -o ./dist/output.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sourceserif' : ['Source Serif Pro'],
+      'synonym' : ['Synonym-Variable'],
+      'ibmplex' : ['IBM Plex Sans']
+    }
+  },
+  plugins: [],
+}
